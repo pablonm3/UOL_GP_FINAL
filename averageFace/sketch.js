2a6644151a05bc550cdce42d5118ad38076e2cc4
pablo marino <pablo-n-m@hotmail.com>
2022-01-31 15:55:48 -0300

    add lerp to avg faces

--- averageFace/sketch.js
+++ averageFace/sketch.js
@@ -1,6 +1,9 @@
 var imgs = [];
 var avgImg;
 var numOfImages = 30;
+var amt = 0
+var img_index;
+
 
 //////////////////////////////////////////////////////////
 function preload() { // preload() runs once
@@ -14,28 +17,31 @@ function preload() { // preload() runs once
 }
 //////////////////////////////////////////////////////////
 function setup() {
+    img_index = int(random(0, numOfImages))
     pixelDensity(1);
     createCanvas(imgs[0].width * 2, imgs[0].height);
     avgImg = createGraphics(imgs[0].width, imgs[0].height );
 }
 
 function keyPressed() {
-    draw();
+    img_index = int(random(0, numOfImages))
+    loop()
+}
+
+function mouseMoved() {
+    amt = map(mouseX, 0, width, 0,1)
+    draw()
 }
 
 //////////////////////////////////////////////////////////
 function draw() {
     background(125);
-    const img_index = int(random(0, numOfImages))
     image(imgs[img_index], 0, 0);
 
     avgImg.loadPixels();
     for(var i=0; i<numOfImages; i++){
         imgs[i].loadPixels();
     }
-    // imgs[0].pixels.forEach((pixel, index)=>{
-    //     const x = index
-    // })
     for(var x =0; x<imgs[0].width; x++){
         for(var y =0; y<imgs[0].width; y++){
             var index = ((imgs[0].width * y) + x) * 4;
@@ -47,9 +53,14 @@ function draw() {
                 sumG += imgs[i].pixels[index+1]
                 sumB += imgs[i].pixels[index+2]
             }
-            avgImg.pixels[index + 0] = sumR/numOfImages // red channel
-            avgImg.pixels[index + 1] = sumG/numOfImages // green channel
-            avgImg.pixels[index + 2] = sumB/numOfImages // blue channel
+            var avg_vector = createVector(sumR/numOfImages, sumG/numOfImages, sumB/numOfImages)
+            const right_img_x = imgs[img_index].pixels[index +0]
+            const right_img_y = imgs[img_index].pixels[index +1]
+            const right_img_z = imgs[img_index].pixels[index +2]
+            avg_vector.lerp(right_img_x, right_img_y, right_img_z, amt)
+            avgImg.pixels[index + 0] = avg_vector.x // red channel
+            avgImg.pixels[index + 1] = avg_vector.y // green channel
+            avgImg.pixels[index + 2] = avg_vector.z // blue channel
             avgImg.pixels[index + 3] = 255
         }   
     }
